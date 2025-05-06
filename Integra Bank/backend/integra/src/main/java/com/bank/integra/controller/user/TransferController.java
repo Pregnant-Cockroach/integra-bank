a7d370488407fa0d6cdca93cfd7c1585d305aac1
AgentVadim839 <149328680+AgentVadim839@users.noreply.github.com>
2025-05-06 13:33:24 +0300

    Transfer improvement
    Added new transaction confirm page to check data. It does not longer create TransferDTO object from view, it sends data to controller and only then correct object will be created.

--- Integra Bank/backend/integra/src/main/java/com/bank/integra/controller/user/TransferController.java
+++ Integra Bank/backend/integra/src/main/java/com/bank/integra/controller/user/TransferController.java
@@ -2,12 +2,14 @@ package com.bank.integra.controller.user;
 
 import com.bank.integra.services.DTO.TransferDTO;
 import com.bank.integra.services.bank.PaymentService;
+import com.bank.integra.services.person.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/user")
 @Controller
@@ -15,10 +17,29 @@ public class TransferController {
     @Autowired
     private PaymentService paymentService;
 
-    //TODO Страница подтверждения операции перевода запилить и валидацию!
+    @Autowired
+    private UserService userService;
+
+    //TODO Запилить и валидацию!
     @PostMapping("/transfer")
-    public String makeTransfer(@ModelAttribute TransferDTO transferDTO, Model model) {
-        paymentService.makePayment(transferDTO.getSenderId(), transferDTO.getRecipientId(), transferDTO.getAmount(), model);
-        return "redirect:/user/home";
+    public String makeTransfer(@RequestParam Integer senderId,
+                               @RequestParam Integer recipientId,
+                               @RequestParam Double amount, Model model) {
+
+        TransferDTO transferDTO = new TransferDTO(senderId, recipientId, amount,
+                userService.getUserDetailsByUserId(senderId).getBalance(),
+                userService.getUserDetailsByUserId(recipientId).getFirstName(),
+                userService.getUserDetailsByUserId(recipientId).getLastName());
+
+        model.addAttribute("transferData", transferDTO);
+        return "confirmPayment";
+    }
+
+    @PostMapping("confirm-transfer")
+    public String confirmTransfer(@RequestParam Integer senderId,
+                                  @RequestParam Integer recipientId,
+                                  @RequestParam Double amount, Model model) {
+        paymentService.makePayment(senderId, recipientId, amount, model);
+        return "redirect:/user/home?transactionSuccess=true";
     }
 }
--- Integra Bank/backend/integra/src/main/java/com/bank/integra/services/DTO/TransferDTO.java
+++ Integra Bank/backend/integra/src/main/java/com/bank/integra/services/DTO/TransferDTO.java
@@ -1,10 +1,37 @@
 package com.bank.integra.services.DTO;
 
+import com.bank.integra.entities.details.UserDetails;
+import com.bank.integra.services.person.UserService;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Data
 public class TransferDTO {
     private Integer senderId;
     private Integer recipientId;
     private Double amount;
+    private Double currentBalance;
+    private String recipientName;
+    private String recipientLastName;
+
+    public TransferDTO(Integer senderId, Integer recipientId, Double amount, Double currentBalance, String recipientName, String recipientLastName) {
+        this.senderId = senderId;
+        this.recipientId = recipientId;
+        this.amount = amount;
+        this.currentBalance = currentBalance;
+        this.recipientName = recipientName;
+        this.recipientLastName = recipientLastName;
+    }
+
+    @Override
+    public String toString() {
+        return "TransferDTO{" +
+                "senderId=" + senderId +
+                ", recipientId=" + recipientId +
+                ", amount=" + amount +
+                ", currentBalance=" + currentBalance +
+                ", recipientName='" + recipientName + '\'' +
+                ", recipientLastName='" + recipientLastName + '\'' +
+                '}';
+    }
 }
