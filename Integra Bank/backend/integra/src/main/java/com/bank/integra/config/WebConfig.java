6e77a773e1f019073ee367150589d6f7d2df95ef
AgentVadim839 <149328680+AgentVadim839@users.noreply.github.com>
2025-11-22 13:58:26 +0200

    Revert "Test"
    This reverts commit 284ecf1c9c76486b22b83f4d39505d15cda41882.

--- Integra Bank/backend/integra/src/main/java/com/bank/integra/config/WebConfig.java
+++ Integra Bank/backend/integra/src/main/java/com/bank/integra/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// Временно офф
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final UserStatusInterceptor userStatusInterceptor;
 
